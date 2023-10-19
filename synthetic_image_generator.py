746152c766059ae3d70ffa00f4a9d42f126ead22
kaldan007 <kaldantenzin@gmail.com>
2023-10-19 12:09:11 +0530

    line image and text

--- synthetic_image_generator.py
+++ synthetic_image_generator.py
@@ -4,8 +4,14 @@ from PIL import Image, ImageDraw, ImageFont
 IMAGE_PATH = './images'
 TEXT_PATH = Path('./texts/kangyur')
 FONT_PATHS = {
-    'Uchen': './fonts/monlam_uni_ochan1.ttf',
-    'Chouk': './fonts/monlam_uni_chouk.ttf',
+    'Uchen1': './fonts/monlam_uni_ochan1.ttf',
+    # 'Uchen4': './fonts/monlam_uni_ochan4.ttf',
+    # 'Uchen5': './fonts/monlam_uni_uchen5.ttf',
+    # 'Chouk': './fonts/monlam_uni_chouk.ttf',
+    # 'Drutsa': './fonts/monlam_uni_dutsa1.ttf',
+    # 'Paytsik': './fonts/monlam_uni_paytsik.ttf',
+    # 'Tikrang': './fonts/monlam_uni_tikrang.ttf',
+    # 'Tiktong': './fonts/monlam_uni_tiktong.ttf',
 }
 
 class SyntheticImageGenerator:
@@ -19,6 +25,10 @@ class SyntheticImageGenerator:
     def get_pages(self, vol_text):
         pages = vol_text.split('\n\n')
         return pages
+    
+    def save_line(self, line, base_dir, line_file_name):
+        (base_dir/ f'transcriptions/{line_file_name}.txt').write_text(line, encoding='utf-8')
+
 
     def save_image(self, text, img_file_name):
         font_file_name = FONT_PATHS.get(self.font_type, 'Uchen')
@@ -40,10 +50,14 @@ class SyntheticImageGenerator:
         # Save the image
         img.save(img_file_name)
 
-    def save_images(self, pages, vol_number):
-        for page_number, page in enumerate(pages,1):
-            img_file_name = f"{IMAGE_PATH}/{self.font_type}/v{vol_number:03}/{page_number:04}.jpg"
-            self.save_image(page, img_file_name)
+    def save_images(self, lines, vol_number):
+        line_number = 1
+        for line in lines:
+            if line and line_number<2000:
+                img_file_name = Path(f"{IMAGE_PATH}/{self.font_type}/v{vol_number:03}/{self.font_type}_{line_number:06}.jpg")
+                self.save_line(line, img_file_name.parent, img_file_name.stem)
+                self.save_image(line, img_file_name)
+                line_number += 1
 
     def create_synthetic_data(self):
         vol_paths = list(TEXT_PATH.iterdir())
@@ -51,8 +65,9 @@ class SyntheticImageGenerator:
         for vol_number, vol_path in enumerate(vol_paths, 1):
             Path(f"{IMAGE_PATH}/{self.font_type}/v{vol_number:03}").mkdir(parents=True, exist_ok=True)
             vol_text = vol_path.read_text(encoding='utf-8')
-            pages = self.get_pages(vol_text)
-            self.save_images(pages, vol_number)
+            lines = vol_text.split('\n')
+            self.save_images(lines, vol_number)
+            break
 
 if __name__ == "__main__":
     # synthetic_image_generator = SyntheticImageGenerator(
@@ -62,13 +77,17 @@ if __name__ == "__main__":
     #     font_type="Uchen"
     # )
 
-    # synthetic_image_generator = SyntheticImageGenerator(
-    #     image_width=2000,
-    #     image_height=350,
-    #     font_size=28,
-    #     font_type="Chouk"
-    # )
-
-    synthetic_image_generator.create_synthetic_data()
+    for font_type in FONT_PATHS.keys():
+        # try:
+        synthetic_image_generator = SyntheticImageGenerator(
+            image_width=2000,
+            image_height=80,
+            font_size=30,
+            font_type=font_type
+        )
+        synthetic_image_generator.create_synthetic_data()
+        # except:
+        #     print(f"Error in {font_type}")
+            
     
 
